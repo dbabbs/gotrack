43e9d95e4842586a1571a3af7d33f9c37c69724c
dbabbs <dylan.babbs@gmail.com>
2016-11-29 22:56:48 -0800

    Initial Commit

---
+++ gotrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gotrack
+//
+//  Created by Babbs, Dylan on 11/29/16.
+//  Copyright © 2016 Babbs, Dylan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
