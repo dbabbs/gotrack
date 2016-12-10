44061d18772333031db5965c83249cd88c46b6d3
Keegan Farley <keegomyneego@live.com>
2016-12-10 10:54:50 -0800

    WIP

---
+++ gotrack/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  gotrack
+//
+//  Created by Keegs on 12/6/16.
+//  Copyright © 2016 Babbs, Dylan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func hamburgerButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
