754186c79faa8c2f07f288047bf5c5ad1dffd896
Keegan Farley <keegomyneego@live.com>
2016-12-13 02:49:31 -0800

    updated welcome workflow

--- gotrack/WelcomeViewController.swift
+++ gotrack/WelcomeViewController.swift
@@ -6,43 +6,94 @@
 //  Copyright © 2016 Babbs, Dylan. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
-class WelcomeViewController: UIViewController {
+class WelcomeViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+
+    //------------------------------------------------------------
+    // UIViewController overrides
+    //------------------------------------------------------------
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationManager.delegate = self
+    }
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        if hasLocationPermissions() {
-            goToNextPage()
-        }
+        verifyLocationPermissions()
     }
 
-    //
+    //------------------------------------------------------------
+    // CLLocationManagerDelegate methods
+    //------------------------------------------------------------
 
-    private func requestLocationPermissions() {
-        Log.info("requesting location permissions...")
-
-         // TODO request location permissions here
-
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        Log.error(in: self, because: error.localizedDescription)
     }
 
-
-    private func hasLocationPermissions() -> Bool {
-
-        // TODO check if we have location permissions
-
-        return true
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        Log.info("authorization status changed to \(authorizationDescription(for: status))")
+        verifyLocationPermissions()
     }
 
-    // 
+    //------------------------------------------------------------
+    // IBAction methods
+    //------------------------------------------------------------
 
     @IBAction func nextButtonPressed(_ sender: UIButton) {
         requestLocationPermissions()
+    }
 
-        if hasLocationPermissions() {
+    //------------------------------------------------------------
+    // Private methods
+    //------------------------------------------------------------
+
+    /// request location permissions
+    private func requestLocationPermissions() {
+        Log.info("requesting location permissions...")
+        Log.info("current authorization status: \(authorizationDescription())")
+
+        locationManager.requestAlwaysAuthorization()
+    }
+
+    // get some descriptive text about the current auth status
+    private func authorizationDescription(for status: CLAuthorizationStatus = CLLocationManager.authorizationStatus()) -> String {
+        var description: String = "unknown"
+
+        switch status {
+        case CLAuthorizationStatus.authorizedAlways: description = "authorizedAlways"
+        case CLAuthorizationStatus.authorizedWhenInUse: description = "authorizedWhenInUse"
+        case CLAuthorizationStatus.denied: description = "denied"
+        case CLAuthorizationStatus.notDetermined: description = "notDetermined"
+        case CLAuthorizationStatus.restricted: description = "restricted"
+        }
+
+        return description
+    }
+
+
+    /// check if we have location permissions
+    private func verifyLocationPermissions() {
+        guard CLLocationManager.locationServicesEnabled() else {
+            Log.error(in: self, because: "location services are disabled")
+            return
+        }
+
+        switch CLLocationManager.authorizationStatus() {
+        case CLAuthorizationStatus.authorizedAlways:
             goToNextPage()
-        } else {
+
+        case CLAuthorizationStatus.notDetermined:
+            break
+
+        default:
+            Log.info("authorization status not always authorized")
             goToRequestPermissionsPage()
         }
     }
